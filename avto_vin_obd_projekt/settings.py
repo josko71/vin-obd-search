912dadf16411a8c6881214e2bfdecfd9b931f623
josko <josko@kali.localhost>
2025-08-03 17:24:25 +0200

    popravek ALLOWED_HOST v settings.py

--- avto_vin_obd_projekt/settings.py
+++ avto_vin_obd_projekt/settings.py
@@ -14,8 +14,12 @@ SECRET_KEY = config('SECRET_KEY', default='nek-privzet-kljuc-za-razvoj-ni-varno-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+
 # Dovoljeni gostitelji za lokalni razvoj in Railway
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'vin-obd-search-production.up.railway.app']
 
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1, .localhost, .railway.app').split(',')
 
