f4b47eb8823d2b1aafc18f8ff839e1a944a3b514
josko <josko@kali.localhost>
2025-08-04 19:11:35 +0200

    popravljena lokalna aplikacija II

--- avto_vin_obd_projekt/urls.py
+++ avto_vin_obd_projekt/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('iskanje/', include('vozila.urls')), # Spremenite to vrstico
+    path('', include('vozila.urls')), # Spremenite to vrstico
     path('ajax/get_models/', vozila_views.get_models_ajax, name='get_models_ajax'),
 ]
 
