82dc9a9ee41392fb32e001a681fa77d18f935e12
josko <josko@kali.localhost>
2025-08-12 12:05:01 +0200

    superuser

--- vozila/management/commands/createsu.py
+++ vozila/management/commands/createsu.py
@@ -6,11 +6,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        if not User.objects.filter(username='josko').exists():
+        if not User.objects.filter(username='user').exists(): #popravi username
             User.objects.create_superuser(
-                username='josko',
-                email='josko71@gmail.com',
-                password='196Guer'
+                username='user', #popravi username
+                email='mail', #popravi email
+                password='geslo' #popravi geslo
             )
             self.stdout.write(self.style.SUCCESS('Superuser created'))
         else:
