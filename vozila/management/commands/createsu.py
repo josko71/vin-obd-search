fa9a2c6415703eff5c13346a83fd23e0df1eccb7
josko <josko@kali.localhost>
2025-08-12 11:19:38 +0200

    Dodano python-decouple v requirements.txt

---
+++ vozila/management/commands/createsu.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Creates a superuser'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='josko',
+                email='josko71@gmail.com',
+                password='196Guer'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
