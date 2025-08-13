7ddcdb2b320588cd6b094eeb1d8b7e87ef73196e
josko <josko@kali.localhost>
2025-08-13 19:26:17 +0200

    posodobitev base.py za medijske datoteke

--- avto_vin_obd_projekt/settings/base.py
+++ avto_vin_obd_projekt/settings/base.py
@@ -47,7 +47,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media' / 'ilustracije'
 
 TEMPLATES = [
     {
