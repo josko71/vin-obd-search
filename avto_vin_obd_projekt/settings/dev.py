8ec1b4ac9bfa15800cfc12026ebd990a459f77cb
josko <josko@kali.localhost>
2025-08-13 09:37:18 +0200

    de.py modified #

--- avto_vin_obd_projekt/settings/dev.py
+++ avto_vin_obd_projekt/settings/dev.py
@@ -13,9 +13,9 @@ DATABASES = {
 }
 
 # Odstranite te vrstice:
-INSTALLED_APPS += ['debug_toolbar']
-MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
-INTERNAL_IPS = ['127.0.0.1']
+#INSTALLED_APPS += ['debug_toolbar']
+#MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+#INTERNAL_IPS = ['127.0.0.1']
 
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda request: True
