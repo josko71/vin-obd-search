b72c3bbab81ab9faaa07e61a12e3b9796bc0df8d
josko <josko@kali.localhost>
2025-08-15 10:38:17 +0200

    S3 AWS

--- avto_vin_obd_projekt/settings/base.py
+++ avto_vin_obd_projekt/settings/base.py
@@ -5,14 +5,9 @@ from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Osnovne nastavitve
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = False
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
-ROOT_URLCONF = 'avto_vin_obd_projekt.urls'
-WSGI_APPLICATION = 'avto_vin_obd_projekt.wsgi.application'
+# ... (ostale nastavitve, kot so SECRET_KEY, DEBUG, itd.)
 
-# Aplikacije in middleware
+# Aplikacije
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -21,51 +16,30 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vozila',
+    'storages', # <--- Pomembno
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-# Media files morajo biti definirani pred STATICFILES_DIRS
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'  
+# ... (ostale nastavitve, kot so MIDDLEWARE, TEMPLATES, itd.)
 
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    MEDIA_ROOT,
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # <--- Samo ta vrstica
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+# Nastavitve za AWS S3
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='eu-central-1')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
 
-# Nastavitve baze podatkov
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+# Django zdaj uporablja AWS S3 kot privzeti sistem za shranjevanje datotek
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Povemo Djangoju, kakšen bo osnovni URL za medije, ki jih streže AWS.
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
+
+# ... (ostala koda)
--- avto_vin_obd_projekt/urls.py
+++ avto_vin_obd_projekt/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from vozila import views as vozila_views
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,12 +9,6 @@ urlpatterns = [
     path('ajax/get_models/', vozila_views.get_models_ajax, name='get_models_ajax'),
 ]
 
-# Ta blok zagotavlja serviranje STATIC/MEDIA datotek tudi v produkcijskem okolju,
-# kar je nujno na platformah, kot je Railway, če ne uporabljate namenskega CDN-ja.
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Del za Debug toolbar ostane v if settings.DEBUG, saj ga potrebujemo samo v razvoju.
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
     urlpatterns = [
