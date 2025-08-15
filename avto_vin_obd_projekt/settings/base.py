1759aab12773170c48930ce9056fbd689f3d76da
josko <josko@kali.localhost>
2025-08-15 10:50:05 +0200

    S3 AWS

--- avto_vin_obd_projekt/settings/base.py
+++ avto_vin_obd_projekt/settings/base.py
@@ -3,12 +3,20 @@ from pathlib import Path
 import dj_database_url
 from decouple import config, Csv
 
-ROOT_URLCONF = 'avto_vin_obd_projekt.urls'
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# ... (ostale nastavitve, kot so SECRET_KEY, DEBUG, itd.)
+# =============================================================
+# Osnovne nastavitve
+# =============================================================
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = False
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ROOT_URLCONF = 'avto_vin_obd_projekt.urls'
+WSGI_APPLICATION = 'avto_vin_obd_projekt.wsgi.application'
 
-# Aplikacije
+# =============================================================
+# Aplikacije in Middleware
+# =============================================================
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -17,18 +25,60 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vozila',
-    'storages', # <--- Pomembno
+    'storages',  # Dodano za AWS S3
 ]
 
-# ... (ostale nastavitve, kot so MIDDLEWARE, TEMPLATES, itd.)
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-# Static files
+# =============================================================
+# Predloge (Templates)
+# =============================================================
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# =============================================================
+# Baza podatkov
+# =============================================================
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
+# =============================================================
+# Statične datoteke (CSS, JS)
+# =============================================================
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # <--- Samo ta vrstica
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# Nastavitve za AWS S3
+# =============================================================
+# Medijske datoteke (Slike) - AWS S3
+# =============================================================
+# Vse nastavitve za medije so zdaj usmerjene na AWS S3.
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
@@ -42,5 +92,3 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Povemo Djangoju, kakšen bo osnovni URL za medije, ki jih streže AWS.
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
-
-# ... (ostala koda)
