5fdccb8b5c6c0476cc8a89e688017d7a184a34a2
josko <josko@kali.localhost>
2025-08-15 17:53:31 +0200

    AWS

--- avto_vin_obd_projekt/settings/base.py
+++ avto_vin_obd_projekt/settings/base.py
@@ -83,10 +83,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='eu-central-1')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_VERIFY = True
+#AWS_S3_VERIFY = True
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 # Nastavite Django, da za medije uporablja storitev S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
