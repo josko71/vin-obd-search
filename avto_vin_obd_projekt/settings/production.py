65c0651a6b1988fad0bf40c52d664dccb95696d4
josko <josko@kali.localhost>
2025-08-16 18:07:47 +0200

    Združim prod.py in production.py, odstranim MEDIA_ROOT

--- avto_vin_obd_projekt/settings/production.py
+++ avto_vin_obd_projekt/settings/production.py
@@ -1,9 +1,36 @@
 # settings/production.py
 from .base import *
-from decouple import config
+from decouple import config, Csv
+import os
+import dj_database_url
+
+# ===== Splošne nastavitve =====
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)  # False v produkciji
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='https://*.railway.app', cast=Csv())
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 30 * 24 * 60 * 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+# ===== Baza podatkov =====
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    )
+}
 
 # ===== Medijske datoteke (Slike) - AWS S3 =====
-# Vse nastavitve za medije so zdaj usmerjene na AWS S3.
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
@@ -12,35 +39,34 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_VERIFY = True
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-# Nastavite Django, da za medije uporablja storitev S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
-
-# URL, na katerega Django kaže, da bi našel medijske datoteke
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/ilustracije/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Odstrani MEDIA_ROOT, ker ni potreben za S3
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# ===== Logging =====
 LOGGING = {
-      'version': 1,
-      'disable_existing_loggers': False,
-      'handlers': {
-          'console': {
-              'class': 'logging.StreamHandler',
-          },
-      },
-      'loggers': {
-          'django': {
-              'handlers': ['console'],
-              'level': 'INFO',
-          },
-          'storages': {
-              'handlers': ['console'],
-              'level': 'DEBUG',
-              'propagate': False,
-          },
-          'boto3': {
-              'handlers': ['console'],
-              'level': 'DEBUG',
-              'propagate': False,
-          },
-      },
-  }
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'storages': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'boto3': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
