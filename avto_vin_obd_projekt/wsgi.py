cdcc35596da803bec142a3a2138902bfae49716d
josko <josko@kali.localhost>
2025-08-16 19:04:27 +0200

    start.sh

--- avto_vin_obd_projekt/wsgi.py
+++ avto_vin_obd_projekt/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+print(f"Loading settings from WSGI: {os.environ.get('DJANGO_SETTINGS_MODULE', 'Not set')}")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avto_vin_obd_projekt.settings.production')
